e898d546ff902fe8fdfcc7f47c0d7fe3df150e31
Dennis Smink <dennissmink@gmail.com>
2020-08-25 14:52:03 +0200

    Add zip script

---
+++ zip.sh
@@ -0,0 +1,13 @@
+zip -r ploi-wordpress-plugin.zip \
+    assets \
+    inc \
+    languages \
+    resources \
+    example.gif \
+    LICENSE \
+    mix-manifest.json \
+    package.json \
+    ploi-wordpress-plugin.php \
+    README.md \
+    tailwind.config.json \
+    webpack.mix.js
